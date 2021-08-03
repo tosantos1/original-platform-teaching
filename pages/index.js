b1e77112298c5f330ab1de56f9813b1749f6e944
Calixoto <grimmjow006@live.com>
2021-08-02 22:32:04 -0300

    consegui

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
           >
             <h3>Deploy &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              asInstantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
         </div>
