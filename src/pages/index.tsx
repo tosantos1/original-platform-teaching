4ae656cd524555a4e2d7e3b3c7894d5066f20aab
Tiago Oliveira <tiagolivesan@outlook.com>
2021-08-04 15:06:58 -0300

    Add folder 'service', add stripe API value

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,41 @@
+import { GetStaticProps } from 'next'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
-      <h1 className={styles.hero}>Hello World</h1>
+      <h1 className={styles.hero}>Valor do curso: {product.amount}</h1>
+      <button className={styles.button}> Comprar curso</button>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JKo2CFV1XJsSmXh3ERzVgqg', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
