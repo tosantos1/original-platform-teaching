3432153a55b6e1808f8865d8c6e8dd85aeef7dc8
Tiago Oliveira <tiagolivesan@outlook.com>
2021-08-23 13:05:59 -0300

    Reponsive page home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,6 @@ import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
-
     return (
         <header className={styles.container}>
             <div className={styles.content}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,19 +17,21 @@ export default function Home({ product }: HomeProps) {
 
   return (
     <>
-
       <main className={styles.container}>
         <section className={styles.hero}>
           <span>
             <img src="/images/raposa.svg" alt="raposa" />
             Oi, bem vindo</span>
-          <h1>Aprendendo no presente <br /> as tecnologias do futuro</h1>
-          <p>Para acessar ao curso é <br /> por apenas <span>{product.amount}</span></p>
+          <h1>Aprendendo no presente as tecnologias do futuro</h1>
+          <p>Para acessar ao curso é por apenas <span>{product.amount}</span></p>
           <BuyButton priceId={product.priceId} />
         </section>
         <section className={styles.lorem}>
           <img src="/images/lorem.svg" alt="lorem" />
           <img src="/images/lorem.svg" alt="lorem" />
+        </section>
+        <section className={styles.loremMobile}>
+          <img src="/images/lorem-v.svg" alt="lorem" />
         </section>
       </main>
     </>
